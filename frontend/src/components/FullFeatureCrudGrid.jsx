b1e92c48b61284224aaa19dea26c85aa26e953a3
RIyadh Braille Anthony Ligan <riyadhbrailleanthonyligan@gmail.com>
2024-02-25 14:15:10 +0800

    dynamic add row

--- frontend/src/components/FullFeatureCrudGrid.jsx
+++ frontend/src/components/FullFeatureCrudGrid.jsx
@@ -30,44 +30,6 @@ const randomRole = () => {
     return randomArrayItem(roles);
 };
 
-const initialRows = [
-    {
-        id: randomId(),
-        name: randomTraderName(),
-        age: 25,
-        joinDate: randomCreatedDate(),
-        role: randomRole(),
-    },
-    {
-        id: randomId(),
-        name: randomTraderName(),
-        age: 36,
-        joinDate: randomCreatedDate(),
-        role: randomRole(),
-    },
-    {
-        id: randomId(),
-        name: randomTraderName(),
-        age: 19,
-        joinDate: randomCreatedDate(),
-        role: randomRole(),
-    },
-    {
-        id: randomId(),
-        name: randomTraderName(),
-        age: 28,
-        joinDate: randomCreatedDate(),
-        role: randomRole(),
-    },
-    {
-        id: randomId(),
-        name: randomTraderName(),
-        age: 23,
-        joinDate: randomCreatedDate(),
-        role: randomRole(),
-    },
-];
-
 function EditToolbar(props) {
     const { setRows, setRowModesModel } = props;
 
@@ -137,7 +99,7 @@ function EditToolbar(props) {
 }
 
 export default function FullFeaturedCrudGrid() {
-    const [rows, setRows] = React.useState(initialRows);
+    const [rows, setRows] = React.useState([]);
     const [rowModesModel, setRowModesModel] = React.useState({});
     const apiRef = React.useRef(null);
 
