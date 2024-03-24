815d0ac259d7e1b2691b9c606e77214768b79d8c
RIyadh Braille Anthony Ligan <riyadhbrailleanthonyligan@gmail.com>
2024-03-24 11:50:03 +0800

    image import

--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -5,13 +5,14 @@ import SignOutButton from '../components/SignOutButton';
 import { Box, Typography } from '@mui/material';
 import pmpLogo2 from './../assets/pmpLogo2.png'
 import microsoftLogo from './../assets/microsoft.png'
+import rafiLogo from './../assets/rafiLogo.png'
 
 const LoginPage = () => {
     const isAuthenticated = useIsAuthenticated();
 
     return (
         <Box sx={{ position: 'relative', height: '100vh', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
-            <img src='src/assets/rafiLogo.png' style={{ width: '450px', height: '300px', position: 'absolute', top: 0, left: 0 }} alt="RAFI Logo" />
+            <img src={rafiLogo} style={{ width: '450px', height: '300px', position: 'absolute', top: 0, left: 0 }} alt="RAFI Logo" />
             <img src={pmpLogo2} style={{ width: '350px', height: '400px', position: 'absolute', top: -20, right: 0}} alt="RAFI Logo" />
             <Box sx={{ backgroundColor: '#fff', p: 4, textAlign: 'center', borderRadius: 4 }}>
                 <Box mb={4}>
